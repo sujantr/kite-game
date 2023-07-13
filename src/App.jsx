f050d0fb3aecc2ae92a4f781c4a7e96c03f76af4
sujantr <sushan.shrestha640@gmail.com>
2023-07-13 19:38:50 +0545

    push

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+import * as THREE from 'three';
+
+import SceneInit from './lib/SceneInit';
+
+function App() {
+  useEffect(() => {
+    const test = new SceneInit('threeJsCanvas');
+    test.initialize();
+    test.animate();
+
+    const boxGeometry = new THREE.BoxGeometry(15, 15, 15);
+    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    test.scene.add(boxMesh);
+  }, [])
+  return (
+    <>
+  <div>
+    <canvas id="threeJsCanvas"></canvas>
+  </div>
+    </>
+  )
+}
+
+export default App
